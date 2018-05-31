390c32e4a49298882d4c9c89630fb1b003643188
Joan Marc Carbo Arnau <joanmarc@gmail.com>
2018-05-31 17:10:38 +0200

    Added namespaces

--- secrets.sh
+++ secrets.sh
@@ -1,4 +1,9 @@
-kubectl -n kube-system create secret tls imimcloud --key=certs/imimcloud.key --cert=certs/fullchain.cer
-kubectl -n kube-system create secret tls imimcat --key=certs/imim.cat.key --cert=certs/imim.cat.all.pem
-kubectl -n kube-system create secret tls fimimcat --key=certs/fimim.cat.key --cert=certs/fimim.cat.all.pem
-kubectl -n kube-system create secret tls imimes --key=certs/imim.es.key --cert=certs/imim.es.all.pem
+#kubectl --kubeconfig=/Users/joanmarc/.kube/config.production2 -n kube-system create secret tls imimcloud --key=certs/imimcloud.key --cert=certs/fullchain.cer
+#kubectl --kubeconfig=/Users/joanmarc/.kube/config.production2 -n kube-system create secret tls imimcat --key=certs/imim.cat.key --cert=certs/imim.cat.all.pem
+#kubectl --kubeconfig=/Users/joanmarc/.kube/config.production2 -n kube-system create secret tls fimimcat --key=certs/fimim.cat.key --cert=certs/fimim.cat.all.pem
+#kubectl --kubeconfig=/Users/joanmarc/.kube/config.production2 -n kube-system create secret tls imimes --key=certs/imim.es.key --cert=certs/imim.es.all.pem
+#
+kubectl --kubeconfig=/Users/joanmarc/.kube/config.production2 -n default create secret tls imimcloud --key=certs/imimcloud.key --cert=certs/fullchain.cer
+kubectl --kubeconfig=/Users/joanmarc/.kube/config.production2 -n default create secret tls imimcat --key=certs/imim.cat.key --cert=certs/imim.cat.all.pem
+kubectl --kubeconfig=/Users/joanmarc/.kube/config.production2 -n default create secret tls fimimcat --key=certs/fimim.cat.key --cert=certs/fimim.cat.all.pem
+kubectl --kubeconfig=/Users/joanmarc/.kube/config.production2 -n default create secret tls imimes --key=certs/imim.es.key --cert=certs/imim.es.all.pem
